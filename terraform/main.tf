bb2cd3534dfc83c4d693ad98199a3c546a537de1
Nandeeswara Reddy <rajnandhube@gmail.com>
2025-11-17 23:01:16 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,39 +7,3 @@ resource "multipass_instance" "controlplane" {
     image    = "24.04"
 }
 
-resource "multipass_instance" "workernodes" {
-    name     = "workernode-1"
-    cpus     = 2
-    memory   = "2G"
-    disk     = "20G"
-    image    = "24.04"
-}
-
-resource "multipass_instance" "workernodes1" {
-    name     = "workernode-2"
-    cpus     = 2
-    memory   = "2G"
-    disk     = "10G"
-    image    = "24.04"    
-}
-
-
-# Create 2 worker nodes using count
-# resource "multipass_instance" "workernodes" {
-#     count    = 2
-#     name     = "workernode-${count.index + 01}"
-#     cpus     = 2
-#     memory   = "2G"
-#     disk     = "20G"
-#     image    = "24.04"
-# }
-
-
-# resource "multipass_instance" "worker-nodes" {
-#     for_each = toset(["workernode-01", "worker-node02"])
-#     name     = each.key
-#     cpus     = 2
-#     memory   = "2G"
-#     disk     = "20G"
-#     image    = "24.04"
-# }
